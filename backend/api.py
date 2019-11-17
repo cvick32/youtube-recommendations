4d5604e7c5a9e5929045508c6ae48cd82ca467dd
cole <cole.d.vick@gmail.com>
2019-11-17 14:02:00 -0500

    quick api endpoint for the extension

---
+++ backend/api.py
@@ -0,0 +1,27 @@
+from flask import Flask, json, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
+
+videos = list()
+
+@app.route('/recs', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
+def get_companies():
+  req_data = request.get_json()
+  next_vid = req_data["upNext"]
+  videos.append(next_vid)
+  record_vids()
+  return json.dumps({"success": True}), 201
+
+def record_vids():
+  video_log = open("data/videos.json", "w+")
+  video_log.write(json.dumps(videos) + "\n")
+  video_log.close()
+
+if __name__ == '__main__':
+  app.run(debug=True, port=8000)
+
+
