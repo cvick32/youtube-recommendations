ee3c57d352184bf6ed9294b9b25a59417f6a6c51
cole <cole.d.vick@gmail.com>
2019-11-21 12:49:45 -0500

    Added looping to grab videos on load of new video
    - added videoScrapeLoop to constantly look for new videos
      - waits for 5 seconds each time to check if a new video has been
      loaded
      - this solves the "refresh problem" mentioned in the README
      - we can now begin to get more data from the page, probably adhering
      to the JSON structure listed in `backend/api.py`

--- extension/logger.js
+++ extension/logger.js
@@ -1,31 +1,35 @@
 var relatedVideosMax = 5;
 
-var relatedVideosSelector = "div#secondary > #secondary-inner > #related > ytd-watch-next-secondary-results-renderer > #items";
-var upNextSelector = relatedVideosSelector + " > ytd-compact-autoplay-renderer > div#contents > ytd-compact-video-renderer > div#dismissable > ytd-thumbnail > a#thumbnail";
+var relatedVideos = "div#secondary > #secondary-inner > #related > ytd-watch-next-secondary-results-renderer > #items";
+var currentVideoTitle = "div#primary > #primary-inner > #info > #info-contents > ytd-video-primary-info-renderer  #container > h1 > yt-formatted-string";
+var currentChannelName = "#channel-name > #container > #text-container > yt-formatted-string > a";
+// document.querySelector("#channel-name > #container > #text-container > yt-formatted-string > a").text
+
+var upNext = relatedVideos + " > ytd-compact-autoplay-renderer > div#contents > ytd-compact-video-renderer > div#dismissable > ytd-thumbnail > a#thumbnail";
 
 /**
  * Gets the recommended videos on a given page up to maximum, 
  * defined above
  */
 function getRecommendedVideos() {
-  console.log("RECOMMENDATIONS")
-  var upNextLink = document.querySelector(upNextSelector).href;
-  var recommendedLinks = [];
-  // must start from 3
-  for (video = 3; video < relatedVideosMax + 3; video++) {
-    try {
-      recommendedLinks.push(document.querySelector(selectorForVideo(video)).href);
-    } catch (err) {
-      console.log(err);
+    console.log("RECOMMENDATIONS")
+    var upNextLink = document.querySelector(upNext).href;
+    var recommendedLinks = [];
+    // must start from 3
+    for (video = 3; video < relatedVideosMax + 3; video++) {
+        try {
+            recommendedLinks.push(document.querySelector(selectorForVideo(video)).href);
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
-  console.log(upNextLink);
-  console.log(recommendedLinks);
-  toSend = {
-    "upNext": upNextLink,
-    "recommended": recommendedLinks
-  }
-  sendAPIRequest(toSend);
+    console.log(upNextLink);
+    console.log(recommendedLinks);
+    toSend = {
+        "upNext": upNextLink,
+        "recommended": recommendedLinks
+    }
+    sendAPIRequest(toSend);
 }
 
 /**
@@ -33,16 +37,16 @@ function getRecommendedVideos() {
  * @param jsonBody 
  */
 function sendAPIRequest(jsonBody) {
-  const req = new XMLHttpRequest();
-  const url='http://127.0.0.1:8000/recs';
-  req.open("POST", url);
-  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  req.setRequestHeader('Access-Control-Allow-Origin', '*');
-  req.send(JSON.stringify(jsonBody));
+    const req = new XMLHttpRequest();
+    const url = 'http://127.0.0.1:8000/recs';
+    req.open("POST", url);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.setRequestHeader('Access-Control-Allow-Origin', '*');
+    req.send(JSON.stringify(jsonBody));
 
-  req.onreadystatechange = (e) => {
-    console.log(req.responseText)
-  }
+    req.onreadystatechange = (e) => {
+        console.log(req.responseText)
+    }
 }
 
 /**
@@ -50,7 +54,35 @@ function sendAPIRequest(jsonBody) {
  * @param {index in recommended videos} videoIndex 
  */
 function selectorForVideo(videoIndex) {
-  return relatedVideosSelector + ` > ytd-compact-video-renderer:nth-child(${videoIndex}) > div#dismissable > ytd-thumbnail > a#thumbnail`;
+    return relatedVideos + ` > ytd-compact-video-renderer:nth-child(${videoIndex}) > div#dismissable > ytd-thumbnail > a#thumbnail`;
+}
+/**
+ * Sleeps for `time_ms`
+ * @param {timeout in milliseconds} time_ms 
+ */
+function sleep(time_ms) {
+    return new Promise(resolve => setTimeout(resolve, time_ms));
+}
+
+/**
+ * Runs the recommended video scrape for `video_limit`
+ * number of videos
+ * @param {int} video_limit 
+ */
+async function videoScrapeLoop(video_limit) {
+    var i = 0;
+    var curUrl = window.location.href;
+    while (i < video_limit) {
+        console.log("in loop");
+        if (curUrl != window.location.href) {
+            console.log("get vids");
+            curUrl = window.location.href;
+            getRecommendedVideos();
+            i++;
+        }
+       await sleep(5000);
+    }
 }
 
 getRecommendedVideos();
+videoScrapeLoop(5);
