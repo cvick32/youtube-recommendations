66273df25148b390a8bdb9041a100b0f01262c6f
cole <cole.d.vick@gmail.com>
2019-11-25 01:45:37 -0500

    added route for html graph and cleaned up title - html route will display the video graph - title still has other data just not as crazy now

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, request
+from flask import Flask, json, request, render_template
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -11,32 +11,24 @@ videos = list()
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def receive_recommendations():
   req_data = request.get_json()
+  for vid in req_data["recommended"]:
+      vid["title"] = " ".join(vid["title"].split())
   videos.append(req_data)
   record_vids()
   return json.dumps({"success": True}), 201
+
+@app.route('/full_graph')
+def full_graph():
+    js_path = "./js/full_graph.js"
+    return render_template('graph.html', context=js_path)
 
 def record_vids():
   video_log = open("data/videos.json", "w+")
   video_log.write(json.dumps(videos) + "\n")
   video_log.close()
 
+
+
 if __name__ == '__main__':
   app.run(debug=True, port=8000)
 
-"""
-toSend = {
-        "name": curVideo,
-        "channel": curChannel,
-        "link": curUrl,
-        "up_next_link": upNextLink,
-        "recommended": [recVideoStruct]
-    }
-"""
-
-"""
-recVideoStruct = {
-        "title": "",
-        "channel": "",
-        "link": ""
-    }
-"""
